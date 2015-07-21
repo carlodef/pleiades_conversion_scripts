0a9abb013c203e1ecde8860db48a3353c4fd1f4b
Carlo De Franchis <carlodef@gmail.com>
2015-07-21 19:46:42 +0200

    fixed vips 16bits script

--- create_16bits_dzi_from_tiff.sh
+++ create_16bits_dzi_from_tiff.sh
@@ -2,16 +2,16 @@
 
 # input TIF file, output DZI file
 TIF=$1
-DZI=${TIF%%.*}.16BITS.dzi
+DZI=${TIF%%.*}_16BITS.dzi
 
 # input grey levels are between 0 and 4095 (12 bits), stored on 16 bits. To get
 # a 16-bits png file displayable in a web-browser these grey level must be
 # multiplied by 16. For some reason this behavious is obtained with vips by
 # asking a multiplication by 4096.
-vips linear $TIF $TIF.v 4096 0
+vips linear $TIF ${TIF%%.*}.v 4096 0 --vips-progress
 
 # dzi file generation
-vips dzsave $TIF.v $DZI --suffix=.png
+vips dzsave ${TIF%%.*}.v $DZI --suffix=.png --vips-progress
 
 # cleanup
-rm $TIF.v
+rm ${TIF%%.*}.v
